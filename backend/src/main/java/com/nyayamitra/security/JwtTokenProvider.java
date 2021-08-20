ac6c8e81f41462f665a5f27e1e85efc95407cc21
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2021-08-20 14:00:00 +0530

    Backend: Implement JWT token provider
    - Create JwtTokenProvider component
    - Generate JWT tokens with HS512 signature
    - Validate tokens and extract username
    - Configure token expiration (24 hours)

---
+++ backend/src/main/java/com/nyayamitra/security/JwtTokenProvider.java
@@ -0,0 +1,60 @@
+package com.nyayamitra.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+    
+    public String generateToken(UserDetails userDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationMs);
+        
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+    
+    public String getUserNameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+        
+        return claims.getSubject();
+    }
+    
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            // Invalid signature
+        } catch (MalformedJwtException ex) {
+            // Invalid token
+        } catch (ExpiredJwtException ex) {
+            // Expired token
+        } catch (UnsupportedJwtException ex) {
+            // Unsupported token
+        } catch (IllegalArgumentException ex) {
+            // Claims string is empty
+        }
+        return false;
+    }
+    
+    public long getExpirationMs() {
+        return jwtExpirationMs;
+    }
+}
