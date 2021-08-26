fd4c5a224d5a35d0babbf132295c3a416c2ca84c
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2021-08-26 14:30:00 +0530

    Backend: Add authentication DTOs
    - LoginRequest: email and password with validation
    - RegisterRequest: client registration fields
    - Add @NotBlank, @Email, @Size validations
    - Lombok @Data for boilerplate reduction

---
+++ backend/src/main/java/com/nyayamitra/dto/RegisterRequest.java
@@ -0,0 +1,51 @@
+package com.nyayamitra.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Phone is required")
+    private String phone;
+
+    private String city;
+
+    private String state;
+
+    private String pincode;
+
+    private String address;
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+    public String getPincode() { return pincode; }
+    public void setPincode(String pincode) { this.pincode = pincode; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+}
