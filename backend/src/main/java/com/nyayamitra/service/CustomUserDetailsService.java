bb3ac7c56970fe8825c57b1f2017ef5c6394a1e6
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2021-08-29 14:00:00 +0530

    Backend: Implement CustomUserDetailsService
    - Load user by email for Spring Security
    - Convert User to UserDetails with authorities
    - Handle user not found exception

---
+++ backend/src/main/java/com/nyayamitra/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.nyayamitra.service;
+
+import com.nyayamitra.model.User;
+import com.nyayamitra.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOpt = userRepository.findByEmail(email);
+        
+        if (userOpt.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        
+        User user = userOpt.get();
+        
+        return new org.springframework.security.core.userdetails.User(
+            user.getEmail(),
+            user.getPassword(),
+            user.isEnabled(),
+            true, true, true,
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+    
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
