c60b479a51d525460f60c32159a8e9906e9132d6
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2021-09-25 10:00:00 +0530

    Backend: Create Consultation domain model
    - Consultation entity for lawyer bookings
    - ConsultationType: VIDEO, PHONE, IN_PERSON
    - ConsultationStatus: PENDING, CONFIRMED, COMPLETED
    - Payment status tracking

---
+++ backend/src/main/java/com/nyayamitra/model/Consultation.java
@@ -0,0 +1,232 @@
+package com.nyayamitra.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+@Document(collection = "consultations")
+public class Consultation {
+
+    @Id
+    private String id;
+
+    private String clientId;
+
+    private String clientName;
+
+    private String lawyerId;
+
+    private String lawyerName;
+
+    private String title;
+
+    private String description;
+
+    private String caseType;
+
+    private Date scheduledDate;
+
+    private Integer duration;
+
+    private ConsultationStatus status;
+
+    private ConsultationType type;
+
+    private String meetingLink;
+
+    private String notes;
+
+    private Double amount;
+
+    private PaymentStatus paymentStatus;
+
+    private String paymentId;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private String cancellationReason;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getLawyerId() {
+        return lawyerId;
+    }
+
+    public void setLawyerId(String lawyerId) {
+        this.lawyerId = lawyerId;
+    }
+
+    public String getLawyerName() {
+        return lawyerName;
+    }
+
+    public void setLawyerName(String lawyerName) {
+        this.lawyerName = lawyerName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCaseType() {
+        return caseType;
+    }
+
+    public void setCaseType(String caseType) {
+        this.caseType = caseType;
+    }
+
+    public Date getScheduledDate() {
+        return scheduledDate;
+    }
+
+    public void setScheduledDate(Date scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public ConsultationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ConsultationStatus status) {
+        this.status = status;
+    }
+
+    public ConsultationType getType() {
+        return type;
+    }
+
+    public void setType(ConsultationType type) {
+        this.type = type;
+    }
+
+    public String getMeetingLink() {
+        return meetingLink;
+    }
+
+    public void setMeetingLink(String meetingLink) {
+        this.meetingLink = meetingLink;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getCancellationReason() {
+        return cancellationReason;
+    }
+
+    public void setCancellationReason(String cancellationReason) {
+        this.cancellationReason = cancellationReason;
+    }
+
+    public enum ConsultationStatus {
+        PENDING,
+        CONFIRMED,
+        COMPLETED,
+        CANCELLED,
+        NO_SHOW,
+        RESCHEDULED
+    }
+
+    public enum ConsultationType {
+        VIDEO,
+        PHONE,
+        IN_PERSON
+    }
+
+    public enum PaymentStatus {
+        PENDING,
+        PAID,
+        REFUNDED,
+        FAILED
+    }
+}
