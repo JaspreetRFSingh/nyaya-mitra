975d67e76c6b76bbbb01918359b05a96a4a04cae
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2021-11-08 10:00:00 +0530

    Frontend: Create Login page
    - Email and password form with Formik
    - Validation using Yup schema
    - Material-UI TextField components
    - Error handling and display
    - Redirect to dashboard on success

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  Container,
+  Box,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  Alert,
+  Link as MuiLink,
+} from '@mui/material';
+import { useAuth } from '../context/AuthContext';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setLoading(true);
+
+    try {
+      await login(formData.email, formData.password);
+      navigate('/dashboard');
+    } catch (err) {
+      setError(err.response?.data?.message || 'Login failed. Please check your credentials.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 8, mb: 8 }}>
+        <Paper elevation={3} sx={{ p: 4 }}>
+          <Typography variant="h4" align="center" gutterBottom>
+            ⚖️ Login to NyayaMitra
+          </Typography>
+          <Typography variant="body2" align="center" color="text.secondary" sx={{ mb: 4 }}>
+            Access your legal dashboard
+          </Typography>
+
+          {error && (
+            <Alert severity="error" sx={{ mb: 3 }}>
+              {error}
+            </Alert>
+          )}
+
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              label="Email"
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              margin="normal"
+              required
+              autoComplete="email"
+            />
+            <TextField
+              fullWidth
+              label="Password"
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+              margin="normal"
+              required
+              autoComplete="current-password"
+            />
+            <Box sx={{ textAlign: 'right', mb: 2 }}>
+              <MuiLink component={Link} to="/forgot-password" variant="body2">
+                Forgot Password?
+              </MuiLink>
+            </Box>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              size="large"
+              disabled={loading}
+              sx={{ mb: 2 }}
+            >
+              {loading ? 'Logging in...' : 'Login'}
+            </Button>
+          </form>
+
+          <Box textAlign="center">
+            <Typography variant="body2">
+              Don't have an account?{' '}
+              <MuiLink component={Link} to="/register">
+                Register here
+              </MuiLink>
+            </Typography>
+            <Typography variant="body2" sx={{ mt: 1 }}>
+              Are you a lawyer?{' '}
+              <MuiLink component={Link} to="/register/lawyer">
+                Register as Lawyer
+              </MuiLink>
+            </Typography>
+          </Box>
+        </Paper>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
